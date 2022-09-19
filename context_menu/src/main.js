f82909529955df19f4579c584115d7aff1d63a72
devcat-jp <service@devcat.jp>
2022-09-19 19:12:27 +0900

    [update] 画面サイズの変更

--- context_menu/src/main.js
+++ context_menu/src/main.js
@@ -5,8 +5,8 @@ const path = require('path')
 // windowの作成
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 400,
+    height: 300,
     webPreferences: {
         preload: path.join(__dirname, 'preload.js'),
     },
